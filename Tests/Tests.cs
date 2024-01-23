85455876b99e4a1ee4502716c19cda4a1506f7a3
Denis Zdorovtsov <mrtrizer@gmail.com>
2024-01-23 14:55:51 +0900

    Unity package

---
+++ Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿
+using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace Abuksigun.UpScript.Tests    
+{
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
